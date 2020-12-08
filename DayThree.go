3f27c78dac3a0dadb50916e5e9c6002c37345caf
Ewann Gavard <ewann.gavard@ext.leroymerlin.fr>
2020-12-08 13:15:47 +0100

    Day 3 step 2 done

--- DayThree.go
+++ DayThree.go
@@ -15,27 +15,34 @@ func main() {
 		x: 1,
 		y: 1,
 	}
-	fmt.Println(slope1, computeTreeCount(forestLine, slope1))
+	res1 := computeTreeCount(forestLine, slope1)
+	fmt.Println(slope1, res1)
 	slope2 := slopeSettings{
 		x: 3,
 		y: 1,
 	}
-	fmt.Println(slope2, computeTreeCount(forestLine, slope2))
+	res2 := computeTreeCount(forestLine, slope2)
+	fmt.Println(slope2, res2)
 	slope3 := slopeSettings{
 		x: 5,
 		y: 1,
 	}
-	fmt.Println(slope3, computeTreeCount(forestLine, slope3))
+	res3 := computeTreeCount(forestLine, slope3)
+	fmt.Println(slope3, res3)
 	slope4 := slopeSettings{
 		x: 7,
 		y: 1,
 	}
-	fmt.Println(slope4, computeTreeCount(forestLine, slope4))
+	res4 := computeTreeCount(forestLine, slope4)
+	fmt.Println(slope4, res4)
 	slope5 := slopeSettings{
 		x: 1,
 		y: 2,
 	}
-	fmt.Println(slope5, computeTreeCount(forestLine, slope5))
+	res5 := computeTreeCount(forestLine, slope5)
+	fmt.Println(slope5, res5)
+
+	fmt.Println(res1 * res2 * res3 * res4 * res5)
 
 }
 
